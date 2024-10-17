86a5c83fdf5181cf2128b97f39d5c2ca5234e531
Yusuke Kawamoto <novogrammer@gmail.com>
2024-10-18 03:03:30 +0900

    heroとaboutを雑に作る

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
-import './style.scss'
+import './scss/style.scss'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>Hello</div>
+<div class="l-main">
+  <section class="p-section-hero">
+    <h1 class="p-section-hero__title">novogrammer is here.</h1>
+  </section>
+  <section class="p-section-about">
+    <h2 class="p-section-about__title">novogrammer is a programmer.</h2>
+  </section>
+  <section class="p-section-matome"></section>
+  <footer class="p-footer"></footer>
+</div>
 `
 
