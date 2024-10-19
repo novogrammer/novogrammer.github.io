6c5da9c7ad8af2f5ecd68a2b86e5540897c159ac
Yusuke Kawamoto <novogrammer@gmail.com>
2024-10-19 21:52:52 +0900

    一通りコーディング

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,30 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <section class="p-section-about">
     <h2 class="p-section-about__title">novogrammer is a programmer.</h2>
   </section>
-  <section class="p-section-matome"></section>
-  <footer class="p-footer"></footer>
+  <section class="p-section-resources">
+    <div class="p-section-resources__content">
+      <h2 class="p-section-resources__title">Recources</h2>
+      <h3 class="p-section-resources__sub-title">sns</h3>
+      <ul class="p-section-resources__list">
+        <li class="p-section-resources__item"><a href="https://x.com/novogrammer" target="_blank">𝕏</a></li>
+        <li class="p-section-resources__item"><a href="https://bsky.app/profile/novogrammer.bsky.social" target="_blank">Bluesky</a></li>
+        <li class="p-section-resources__item"><a href="https://www.threads.net/@novogrammer" target="_blank">Threads</a></li>
+      </ul>
+      <h3 class="p-section-resources__sub-title">dev</h3>
+      <ul class="p-section-resources__list">
+        <li class="p-section-resources__item"><a href="https://github.com/novogrammer" target="_blank">github</a></li>
+        <li class="p-section-resources__item"><a href="https://www.shadertoy.com/user/novogrammer" target="_blank">shadertoy</a></li>
+        <li class="p-section-resources__item"><a href="https://codepen.io/novogrammer" target="_blank">codepen.io</a></li>
+      </ul>
+      <h3 class="p-section-resources__sub-title">community</h3>
+      <ul class="p-section-resources__list">
+        <li class="p-section-resources__item"><a href="https://scrapbox.io/antenna-osaka/" target="_blank">アンテナ大阪のscrapbox</a></li>
+      </ul>
+    </div>
+  </section>
+  <footer class="p-footer">
+    <div class="p-footer__copyright">&copy; 2024 novogrammer</div>
+  </footer>
 </div>
 `
 
